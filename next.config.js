8b5f44984a1fa3a21a09fff47cf30593c020e120
Joar Karlsson <joar.karlsson@evry.com>
2021-03-04 17:24:15 +0100

    Revert "Test for deploying"
    This reverts commit 8edf04d8f965a91c1f77252b0a5b9125c6519916.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: "/zamplyy/_next/static/images/",
+    },
+  ],
+  {
+    assetPrefix: "/zamplyy/",
+    basePath: "/zamplyy",
   },
-};
+]);
