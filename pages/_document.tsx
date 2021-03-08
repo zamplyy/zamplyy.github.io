fac570ee22fa7d88d55528847663bbd645fef80f
Joar Karlsson <joar.karlsson@evry.com>
2021-03-08 16:38:45 +0100

    works now?

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,13 +15,6 @@ export default class MyDocument extends Document {
             as="style"
             href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&family=Knewave&family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600&display=swap"
           />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&family=Knewave&family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600&display=swap"
-            media="print"
-            // @ts-ignore
-            onLoad="this.media='all'"
-          />
           <noscript>
             <link
               rel="stylesheet"
